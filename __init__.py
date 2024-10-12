613a46a30b1ffeccdf908238ffb6bd288253c955
Martijn Dekkers <martijn@prosperpro.tech>
2024-10-12 10:10:00 +0200

    MartijnDekkers/NeewerLitePython

--- __init__.py
+++ __init__.py
@@ -3,17 +3,24 @@ from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.const import CONF_MAC
+from homeassistant.helpers.typing import ConfigType
 
 from .NeewerLight import NeewerLight
 
 DOMAIN = "neewerlight"
 PLATFORMS = ["light"]
 
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Neewer Light component."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Triones from a config entry."""
+    """Set up Neewer Light from a config entry."""
     instance = NeewerLight(entry.data[CONF_MAC])
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = instance
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.data[DOMAIN][entry.entry_id] = instance
+    
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
